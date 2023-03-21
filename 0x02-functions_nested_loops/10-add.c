8b020a2498fc8a7594ed6dbbcebf2834386d4d2c
emanuel kivindyo <kivindyoemanuel@gmail.com>
2023-03-21 01:40:36 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - prints out the addition of two numbers
+ *@p: first value
+ *@l: second value
+ * Return: sum of two numbers
+ */
+
+int add(int p, int l)
+{
+int i;
+i = p + l;
+return (i);
+}
