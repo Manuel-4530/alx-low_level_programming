63fe00a1b59bb37b71670126a7eb509ebe890714
emanuel kivindyo <kivindyoemanuel@gmail.com>
2023-04-05 10:03:11 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _sqrt - Entry point
+ * Description: Natural square root
+ * @n: Integer
+ * @x: Integer
+ * Return: -1 if negative n if 0 or 1
+ */
+
+int _sqrt(int n, int x)
+{
+	if ((x * x) == n)
+	{
+		return (x);
+	}
+	if (x == n / 2)
+	{
+		return (-1);
+	}
+	return (_sqrt(n, x + 1));
+}
+
+/**
+  * _sqrt_recursion - Calculates natural square root
+  * @n: number to calculate the square root
+  *
+  * Return: the natural square root
+  */
+int _sqrt_recursion(int n)
+{
+	int x = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt(n, x));
+}
