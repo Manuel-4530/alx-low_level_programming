2c5bed74d3a678f36cbb3246eb8a15941db0351a
emanuel kivindyo <kivindyoemanuel@gmail.com>
2023-04-17 00:48:41 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_i;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_i *new_dog(char *name, float age, char *owner);
+void free_dog(dog_i *d);
+
+#endif
