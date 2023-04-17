8381f44bf13f01d810a0eb33b4fea22ff32d260f
emanuel kivindyo <kivindyoemanuel@gmail.com>
2023-04-17 00:57:45 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - variable initialization
+ * @d: pointer
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
